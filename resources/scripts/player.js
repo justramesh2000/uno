111cef5516a3979c874d09ae68658038ddd0cdf0
justramesh2000 <justramesh2000@yahoo.com>
2020-04-22 15:43:33 -0400

    drag option for player card
    changes to player.css and player.js to enable drag

--- resources/scripts/player.js
+++ resources/scripts/player.js
@@ -19,6 +19,8 @@
 	 playerArea.style.visibility="hidden";
 
    socket.on('getcards',function(data,playedCards){
+   	document.onmousedown = startDrag;
+        document.onmouseup = stopDrag;
 
 	data = data.filter(function (el) {
   			return el != null;
@@ -240,3 +242,48 @@
 	  	e.preventDefault();
 		socket.emit('sendMsgToServer', playerName.value +' : said Check');
 	});
+
+	function startDrag(e) {
+        // determine event object
+        if (!e) {
+          var e = window.event;
+        }
+
+        // IE uses srcElement, others use target
+        var targ = e.target ? e.target : e.srcElement;
+
+        if (targ.className != 'playerCardImage') {return};
+        // calculate event X, Y coordinates
+          offsetX = e.clientX;
+          offsetY = e.clientY;
+
+        // assign default values for top and left properties
+        if(!targ.style.left) { targ.style.left='0px'};
+        if (!targ.style.top) { targ.style.top='0px'};
+
+        // calculate integer values for top and left 
+        // properties
+        coordX = parseInt(targ.style.left);
+        coordY = parseInt(targ.style.top);
+        drag = true;
+
+        // move div element
+          document.onmousemove=dragDiv;
+        
+      }
+
+      function stopDrag() {
+        drag=false;
+      }
+
+      function dragDiv(e) {
+        if (!drag) {return};
+        if (!e) { var e= window.event};
+        var targ=e.target?e.target:e.srcElement;
+        // move div element
+        targ.style.left=coordX+e.clientX-offsetX+'px';
+        targ.style.top=coordY+e.clientY-offsetY+'px';
+        return false;
+      }
+
+     
